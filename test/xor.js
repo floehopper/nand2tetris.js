4fe9d09183e38dfd4be5024b739f82dc6c83c222
James Mead <james@floehopper.org>
2014-12-31 18:07:56 +0000

    Implement Xor gate in terms of 2 Nots, 2 Ands and 1 Or gate.

---
+++ test/xor.js
@@ -0,0 +1,40 @@
+var chai = require("chai");
+var expect = chai.expect;
+
+var Xor = require("../lib/xor");
+
+describe("Xor", function() {
+  var xor;
+
+  beforeEach(function() {
+    xor = new Xor();
+  });
+
+  it("sets output low when both inputs are low", function() {
+    xor.setInputValue("a", 0);
+    xor.setInputValue("b", 0);
+    xor.evaluate();
+    expect(xor.getOutputValue("out")).to.eql(0);
+  });
+
+  it("sets output high when one input is high", function() {
+    xor.setInputValue("a", 0);
+    xor.setInputValue("b", 1);
+    xor.evaluate();
+    expect(xor.getOutputValue("out")).to.eql(1);
+  });
+
+  it("sets output high when other input is high", function() {
+    xor.setInputValue("a", 1);
+    xor.setInputValue("b", 0);
+    xor.evaluate();
+    expect(xor.getOutputValue("out")).to.eql(1);
+  });
+
+  it("sets output low when both inputs are high", function() {
+    xor.setInputValue("a", 1);
+    xor.setInputValue("b", 1);
+    xor.evaluate();
+    expect(xor.getOutputValue("out")).to.eql(0);
+  });
+});
