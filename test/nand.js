05b22996ce5d58aaaa2daf11c10f1dee14ec9ba6
James Mead <james@floehopper.org>
2014-12-31 14:19:31 +0000

    Implement primitive Nand gate.

---
+++ test/nand.js
@@ -0,0 +1,40 @@
+var chai = require("chai");
+var expect = chai.expect;
+
+var Nand = require("../lib/nand");
+
+describe("Nand", function() {
+  var nand;
+
+  beforeEach(function() {
+    nand = new Nand();
+  });
+
+  it("sets output high when both inputs are low", function() {
+    nand.setInputValue("a", 0);
+    nand.setInputValue("b", 0);
+    nand.evaluate();
+    expect(nand.getOutputValue("out")).to.eql(1);
+  });
+
+  it("sets output high when one input is low", function() {
+    nand.setInputValue("a", 0);
+    nand.setInputValue("b", 1);
+    nand.evaluate();
+    expect(nand.getOutputValue("out")).to.eql(1);
+  });
+
+  it("sets output high when other input is low", function() {
+    nand.setInputValue("a", 1);
+    nand.setInputValue("b", 0);
+    nand.evaluate();
+    expect(nand.getOutputValue("out")).to.eql(1);
+  });
+
+  it("sets output low when both inputs are high", function() {
+    nand.setInputValue("a", 1);
+    nand.setInputValue("b", 1);
+    nand.evaluate();
+    expect(nand.getOutputValue("out")).to.eql(0);
+  });
+});
