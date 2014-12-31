4cfdb177f8b590564664b5f7abaa0984cb7ce31a
James Mead <james@floehopper.org>
2014-12-31 14:34:37 +0000

    Implement And gate in terms of a Nand and a Not gate.

---
+++ test/and.js
@@ -0,0 +1,40 @@
+var chai = require("chai");
+var expect = chai.expect;
+
+var And = require("../lib/and");
+
+describe("And", function() {
+  var nand;
+
+  beforeEach(function() {
+    and = new And();
+  });
+
+  it("sets output low when both inputs are low", function() {
+    and.setInputValue("a", 0);
+    and.setInputValue("b", 0);
+    and.evaluate();
+    expect(and.getOutputValue("out")).to.eql(0);
+  });
+
+  it("sets output low when one input is low", function() {
+    and.setInputValue("a", 0);
+    and.setInputValue("b", 1);
+    and.evaluate();
+    expect(and.getOutputValue("out")).to.eql(0);
+  });
+
+  it("sets output low when other input is low", function() {
+    and.setInputValue("a", 1);
+    and.setInputValue("b", 0);
+    and.evaluate();
+    expect(and.getOutputValue("out")).to.eql(0);
+  });
+
+  it("sets output high when both inputs are high", function() {
+    and.setInputValue("a", 1);
+    and.setInputValue("b", 1);
+    and.evaluate();
+    expect(and.getOutputValue("out")).to.eql(1);
+  });
+});
