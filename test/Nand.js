989fdc0be0d43a30de5e19d55456a42db15f6331
James Mead <james@floehopper.org>
2015-01-11 15:49:14 +0000

    Avoid explicit call to evaluate() for JS-defined Nand gate.
    This isn't a very general solution, but it's a start at least!

--- test/Nand.js
+++ test/Nand.js
@@ -14,28 +14,24 @@ describe("Nand", function() {
   it("sets output high when both inputs are low", function() {
     nand.setInputValue("a", 0);
     nand.setInputValue("b", 0);
-    nand.evaluate();
     expect(nand.getOutputValue("out")).to.eql(1);
   });
 
   it("sets output high when one input is low", function() {
     nand.setInputValue("a", 0);
     nand.setInputValue("b", 1);
-    nand.evaluate();
     expect(nand.getOutputValue("out")).to.eql(1);
   });
 
   it("sets output high when other input is low", function() {
     nand.setInputValue("a", 1);
     nand.setInputValue("b", 0);
-    nand.evaluate();
     expect(nand.getOutputValue("out")).to.eql(1);
   });
 
   it("sets output low when both inputs are high", function() {
     nand.setInputValue("a", 1);
     nand.setInputValue("b", 1);
-    nand.evaluate();
     expect(nand.getOutputValue("out")).to.eql(0);
   });
 });
