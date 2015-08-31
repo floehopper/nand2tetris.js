cdcadb8fc69bb2a08b87771a81b1aecff8e80d6b
James Mead <james@floehopper.org>
2015-08-31 17:01:07 +0100

    Extract Resolver into separate file

--- test/Foo.spec.js
+++ test/Foo.spec.js
@@ -1,39 +1,13 @@
 var chai = require("chai");
 var expect = chai.expect;
 
-var fs = require("fs");
-var HDLParser = require("../lib/HDLParser");
-var parser = new HDLParser();
-var AST = require("../lib/AST");
-
-var Resolver = function() {
-};
-
-var resolver = new Resolver();
-
-Resolver.prototype.resolvePart = function(name) {
-  var hdl = fs.readFileSync([__dirname, "..", "lib", name + ".hdl"].join("/"));
-  var ast = parser.parse(hdl.toString(), { AST: AST });
-  var definition = ast.toDefinition();
-  definition.resolveImplementation(resolver);
-  return definition;
-}
-
-Resolver.prototype.resolveBuiltin = function(name) {
-  try {
-    definition = require("../lib/" + name);
-  } catch(exception) {
-    if (exception.code != "MODULE_NOT_FOUND") {
-      throw exception;
-    };
-  };
-  return definition;
-}
+var Resolver = require("../lib/Resolver");
 
 describe("And", function() {
   var and;
 
   beforeEach(function() {
+    var resolver = new Resolver();
     var definition = resolver.resolvePart("And");
     and = definition.buildModel();
   });
