d0234f81529230a3b52cd8f2c6e8abe7a8552b95
James Mead <james@floehopper.org>
2015-09-04 23:40:40 +0100

    Improve acceptance test failure message

--- test/Acceptance.spec.js
+++ test/Acceptance.spec.js
@@ -8,7 +8,8 @@ describe("Acceptance Test", function() {
   glob.sync("projects/**/*.tst").forEach(function(tstFilePath) {
     it("runs " + tstFilePath, function() {
       var result = child_process.spawnSync("bin/HardwareSimulator.sh", [tstFilePath]);
-      expect(result.status).to.equal(0, result.stderr.toString());
+      var output = [result.stdout.toString(), result.stderr.toString()].join("\n\n");
+      expect(result.status).to.equal(0, output);
     });
   });
 });
