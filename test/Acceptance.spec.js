be48e7d3d0f00b36cb6e46551fd7afe9c33e182d
James Mead <james@floehopper.org>
2015-09-04 23:33:55 +0100

    Use glob in acceptance test to find test scripts automatically

--- test/Acceptance.spec.js
+++ test/Acceptance.spec.js
@@ -2,11 +2,12 @@ var chai = require("chai");
 var expect = chai.expect;
 
 var child_process = require('child_process')
+var glob = require("glob")
 
 describe("Acceptance Test", function() {
-  [ "Not", "And", "Or" ].forEach(function(gate) {
-    it("runs " + gate + ".tst", function() {
-      var result = child_process.spawnSync("bin/HardwareSimulator.sh", ["projects/01/" + gate + ".tst"]);
+  glob.sync("projects/**/*.tst").forEach(function(tstFilePath) {
+    it("runs " + tstFilePath, function() {
+      var result = child_process.spawnSync("bin/HardwareSimulator.sh", [tstFilePath]);
       expect(result.status).to.equal(0, result.stderr.toString());
     });
   });
